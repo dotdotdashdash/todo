4fa7a4873e52b5ab8c00f5bf9333f10a93c61b93
Ismail <connect.ismail@protonmail.com>
2022-05-04 19:08:34 +0530

    Fetches data from JSON and populates the table

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -2,8 +2,23 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var data= this.responseText;
-      console.log(data);
+      jsonData = JSON.parse(data);
+      populate(jsonData);
     }
 };
 xhttp.open("GET", "./resources/todos.json", true);
 xhttp.send();
+
+function populate(info) {
+  var todoBody = document.getElementById("todoBody");
+  
+  info.forEach(element => {
+
+    let status = element.completed ? "Done" : "Not Done"
+
+    let oneTaskRow = `<tr> <th scope="row">${element.id}</th><td>${element.
+      userId}</td><td>${element.title}</td><td>${status}</td></tr>`
+    todoBody.innerHTML += oneTaskRow;
+
+  });
+}
