488b4961219d583bd63ba5a674d36668a2c80c44
Ismail <connect.ismail@protonmail.com>
2022-05-04 22:09:17 +0530

    Sorts tasks based on user

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,24 +1,36 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var data= this.responseText;
-      jsonData = JSON.parse(data);
-      populate(jsonData);
-    }
-};
-xhttp.open("GET", "./resources/todos.json", true);
-xhttp.send();
+function getJsonData(user) {
 
-function populate(info) {
   var todoBody = document.getElementById("todoBody");
-  
+  todoBody.innerHTML = '';
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var data= this.responseText;
+        jsonData = JSON.parse(data);
+        populate(jsonData,user);
+      }
+  };
+  xhttp.open("GET", "./resources/todos.json", true);
+  xhttp.send();
+}
+
+
+function populate(info,user) {
+  console.log(user)
   info.forEach(element => {
 
-    let status = element.completed ? "Done" : "Not Done"
+    if(user == 0) {
+      var taskRow = `<tr> <th scope="row">${element.id}</th><td>${element.userId}</td><td>${element.title}</td><td>${element.completed}</td></tr>` ; 
+      console.log(taskRow);
 
-    let oneTaskRow = `<tr> <th scope="row">${element.id}</th><td>${element.
-      userId}</td><td>${element.title}</td><td>${status}</td></tr>`
-    todoBody.innerHTML += oneTaskRow;
+    } else if(user == element.userId) {
+      console.log(element.userId, element.id);
+      var taskRow = `<tr> <th scope="row">${element.id}</th><td>${element.userId}</td><td>${element.title}</td><td>${element.completed}</td></tr>` ;
+    } else {
+      return;
+    }
+
+    todoBody.innerHTML += taskRow;
 
   });
 }
