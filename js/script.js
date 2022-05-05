14caa95c7436f2abc5c6c9ac4164b7c2d17e585f
Ismail <connect.ismail@protonmail.com>
2022-05-05 14:15:58 +0530

    Nav Bars, Tables & Redirection
    Completes everything except 5 tasks and Username/Password verification.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ function pageRedirect(submitEvent) {
   location.replace("./todo-page.html");
   console.log(location)
 }
+
+function askToLogin() {
+
+}
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  if(alertPlaceholder.innerText == "") {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert"><i class="fa-solid fa-triangle-exclamation"></i> &nbsp;' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  
+    alertPlaceholder.append(wrapper)
+  } else {
+    console.log("dddddd")
+    alertPlaceholder.removeChild(alertPlaceholder.lastChild)
+
+  }
+
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Sign In with your credentials to view the TO-DO list!', 'danger')
+  })
+}
